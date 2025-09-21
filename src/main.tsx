33ea42e4810f049e554e43f26f2d4866f0410c28
Jnwyk <jan.nowak.personal@gmail.com>
2025-09-21 15:57:59 +0200

    create general template

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+function Main() {
+  return <main></main>;
+}
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export default Main;
